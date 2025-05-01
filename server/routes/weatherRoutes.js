abc42438038d5c1f1f9cdfe67b11f042d5f38b9c
Thomas Shih <tshih17110@gmail.com>
2025-05-01 22:46:50 +0800

    Fix routes for deployment

--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const weatherController = require('../controllers/weatherController');
 const router = express.Router();
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 router.get('/weather', weatherController.getWeatherData);
 
 module.exports = router;
