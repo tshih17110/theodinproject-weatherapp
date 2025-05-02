fc411878f75c4e92d54be1fde01565c959412f06
Thomas Shih <tshih17110@gmail.com>
2025-05-02 23:07:47 +0800

    Fix weatherRoutes

--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const weatherController = require('../controllers/weatherController');
 const router = express.Router();
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
 router.get('/weather', weatherController.getWeatherData);
 
 module.exports = router;
